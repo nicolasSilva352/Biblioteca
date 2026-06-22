17bcb73cf93d2181e82c7dac4a9e279a1c384980
nicolas.silva <nicolas.silva@placeti.com.br>
2026-06-22 13:36:20 -0300

    front end pela metade

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login';
+import { DashboardComponent } from './features/dashboard/dashboard';
+import { CadastroComponent } from './features/cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent }, // ROTA NOVA
+  { path: 'dashboard', component: DashboardComponent }
+];
